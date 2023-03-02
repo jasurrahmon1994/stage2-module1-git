9527d6cb1d92e60d8f057abd6d4827c3ba320b48
Jasur Rahmonov <58405118+jasurrahmon1994@users.noreply.github.com>
2023-03-02 23:10:36 +0500

    Update Repo.java

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/jasurrahmon1994/gitTask";
+    public static String REPO_LINK = "https://github.com/jasurrahmon1994/gitTaskRemake";
 }
